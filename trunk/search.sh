62e8deaf3842c7bbbd88fb27ec5e475bb2101d03
palkopatel@gmail.com <palkopatel@gmail.com@9d5e08b5-75f0-f39a-5868-55aa1c377322>
2014-03-24 22:17:12 +0000

    added search script

---
+++ trunk/search.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+    echo
+    echo
+    echo "Illegal number of parameters. You should put search patterns as args"
+    echo
+    exit 1
+fi
+
+cd db
+../find03/find03.exe --file "$@"
+cd ..
